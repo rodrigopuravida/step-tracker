a3fbd0dfe873364e03ecc55ec0391eb74be1aa38
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-05-16 18:14:18 -0600

    fetching steps and weight

--- Step Tracker/DashBoardView.swift
+++ Step Tracker/DashBoardView.swift
@@ -29,85 +29,84 @@ struct DashBoardView: View {
   @State private var selectedStat: HealthMetricContext = .steps
   var isSteps : Bool { selectedStat == .steps}
 
-    var body: some View {
-        NavigationStack{
-          ScrollView{
-            VStack(spacing: 20) {
+  var body: some View {
+    NavigationStack{
+      ScrollView{
+        VStack(spacing: 20) {
 
-              Picker("Selected Stat", selection: $selectedStat) {
-                ForEach(HealthMetricContext.allCases) {
-                  Text($0.title)
-                }
-              }
-              .pickerStyle(.segmented)
-
-              VStack{
-                NavigationLink(value: selectedStat){
-                  HStack{
-                    VStack(alignment: .leading){
-                      Label("Steps", systemImage: "figure.walk")
-                        .font(.title3.bold())
-                        .foregroundStyle(.pink)
-
-                      Text("Avg: 10k Steps")
-                        .font(.caption)
-                    }
-                    Spacer()
-
-                    Image(systemName: "chevron.right")
-                  }
-                }
-                .foregroundStyle(.secondary)
-                .padding(.bottom,12)
-
-                RoundedRectangle(cornerRadius: 12)
-                  .foregroundStyle(.secondary)
-                  .frame(height: 150)
-              }
-              .padding()
-              .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
-
-              VStack(alignment: .leading){
-                  VStack(alignment: .leading){
-                    Label("Averages", systemImage: "calendar")
-                      .font(.title3.bold())
-                      .foregroundStyle(.pink)
-
-                    Text("Last 28 Days")
-                      .font(.caption)
-                      .foregroundStyle(.secondary)
-                  }
-
-                .padding(.bottom,12)
-
-                RoundedRectangle(cornerRadius: 12)
-                  .foregroundStyle(.secondary)
-                  .frame(height: 240)
-              }
-              .padding()
-              .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+          Picker("Selected Stat", selection: $selectedStat) {
+            ForEach(HealthMetricContext.allCases) {
+              Text($0.title)
             }
           }
+          .pickerStyle(.segmented)
+
+          VStack{
+            NavigationLink(value: selectedStat){
+              HStack{
+                VStack(alignment: .leading){
+                  Label("Steps", systemImage: "figure.walk")
+                    .font(.title3.bold())
+                    .foregroundStyle(.pink)
+
+                  Text("Avg: 10k Steps")
+                    .font(.caption)
+                }
+                Spacer()
+
+                Image(systemName: "chevron.right")
+              }
+            }
+            .foregroundStyle(.secondary)
+            .padding(.bottom,12)
+
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 150)
+          }
           .padding()
-          .task{
-            //await hkManager.addSimulatorData()
-            isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+          .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+
+          VStack(alignment: .leading){
+            VStack(alignment: .leading){
+              Label("Averages", systemImage: "calendar")
+                .font(.title3.bold())
+                .foregroundStyle(.pink)
+
+              Text("Last 28 Days")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            }
+
+            .padding(.bottom,12)
+
+            RoundedRectangle(cornerRadius: 12)
+              .foregroundStyle(.secondary)
+              .frame(height: 240)
           }
-          .navigationTitle("Dashboard")
-          .navigationDestination(for: HealthMetricContext.self) { metric in
-            HealthDataListView(metric: metric)
-          }
-          .sheet(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
-            //fetch health data
-          }, content: {
-            HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
-          })
+          .padding()
+          .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
         }
-        .tint(isSteps  ? .pink : .indigo)
+      }
+      .padding()
+      .task{
+        isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+      }
+      .navigationTitle("Dashboard")
+      .navigationDestination(for: HealthMetricContext.self) { metric in
+        HealthDataListView(metric: metric)
+      }
+      .sheet(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
+        //fetch health data
+      }, content: {
+        HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
+      })
     }
+    .tint(isSteps  ? .pink : .indigo)
+  }
 }
 
 #Preview {
-    DashBoardView()
+  DashBoardView()
     .environment(HealthKitManager())
 }
