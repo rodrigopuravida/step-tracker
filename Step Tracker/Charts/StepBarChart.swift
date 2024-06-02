2896270267e24d8273b500945ada6dd5f09069da
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-06-02 09:32:59 -0700

    weight line chart basic ui

--- Step Tracker/Charts/StepBarChart.swift
+++ Step Tracker/Charts/StepBarChart.swift
@@ -118,5 +118,5 @@ struct StepBarChart: View {
 }
 
 #Preview {
-  StepBarChart(selectedStat: .steps, chartData: HealthMetric.mockData)
+  StepBarChart(selectedStat: .steps, chartData: MockData.steps)
 }
