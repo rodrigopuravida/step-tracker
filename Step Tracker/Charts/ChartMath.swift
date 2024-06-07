71b7040a0119b0e27dccde077b44e69bf8385306
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-06-06 20:29:16 -0700

    weight diff calculation

--- Step Tracker/Charts/ChartMath.swift
+++ Step Tracker/Charts/ChartMath.swift
@@ -24,20 +24,33 @@ struct ChartMath {
       weekdayChartData.append(.init(date: firstValue.date, value: avgSteps))
     }
 
-    /*
-    for metric in sortedByWeekDay {
-      print("Day: \(metric.date.weekdayInt), value: \(metric.value)")
+    return weekdayChartData
+  }
+
+  static func averageDailyWeightDiffs(for weights: [HealthMetric]) -> [WeekdayChartData] {
+
+    var diffValues: [(date: Date, value: Double)] = []
+
+    for i in 1..<weights.count {
+
+      let date = weights[i].date
+      let diff = weights[i].value - weights[i-1].value
+      diffValues.append((date: date, value: diff))
     }
+    let sortedByWeekDay = diffValues.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
+    let weekdayArray = sortedByWeekDay.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
 
-    print("----------")
+    var weekdayChartData: [WeekdayChartData] = []
 
-    for day in weekdayChartData {
-      print("Day: \(day.date.weekdayInt), value: \(day.value)")
+    for array in weekdayArray {
+      guard let firstValue = array.first else {continue}
+      let total = array.reduce(0) {$0 + $1.value }
+      let avgWeightDiff = total/Double(array.count)
+
+      weekdayChartData.append(.init(date: firstValue.date, value: avgWeightDiff))
     }
-     */
-
 
     return weekdayChartData
-
   }
+
 }
